f19cb40e7872a03244cb01ae286f49cd2b293181
4BFC <4bee.code@gmail.com>
2023-07-14 21:45:35 +0900

    lecture section5_1

--- react-commplete-guide/src/components/Expense/Expense.js
+++ react-commplete-guide/src/components/Expense/Expense.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
-
+import ExpensesChart from "./ExpensesChart";
 import Card from "../UI/Card";
 import './Expense.css';
 
@@ -22,6 +22,7 @@ const Expense = (props) => {
       {/* <ExpensesFilter   />  */}
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
