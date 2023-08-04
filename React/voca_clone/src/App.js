e1d985188d2113e10f79a4ea1485632b3503733b
4BFC <4bee.code@gmail.com>
2023-08-04 23:16:03 +0900

    create new voca_clone dir

---
+++ React/voca_clone/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Hello from './components/section1/Hello';
+
+function App() {
+  return (
+    <div className="App">
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
