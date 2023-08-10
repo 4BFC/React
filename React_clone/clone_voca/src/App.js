21c5b67ae665ba4e1744f29a90af99916ac3db50
4BFC <4bee.code@gmail.com>
2023-08-10 22:08:12 +0900

    section10 lecture

--- React_clone/clone_voca/src/App.js
+++ React_clone/clone_voca/src/App.js
@@ -2,14 +2,21 @@ import './App.css';
 import Day from './components/section9/Day';
 import DayList from './components/section9/DayList';
 import Header from './components/section9/Header';
-
+import EmptyPage from './components/section9/EmptyPage';
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <DayList />
-      <Day />
+    <div className='App'>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<DayList />} />
+          <Route path="/day/:day" element={<Day />} />
+          <Route path="*" element={<EmptyPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
+
 export default App;
