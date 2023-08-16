152bff9a5bc37c4e24a18e48c85ef1a0a7caa2c6
4BFC <4bee.code@gmail.com>
2023-08-16 23:25:20 +0900

    lecture section14

--- React_clone/clone_voca/src/components/section9_10/Day.js
+++ React_clone/clone_voca/src/components/section9_10/Day.js
@@ -1,16 +1,31 @@
-import dummy from "../../db/data.json";
 import { useParams } from "react-router-dom";
-import Word from "../section11/Word";
+import Word from "../section11_12/Word";
+import { useEffect, useState } from "react";
+import useFetch from "../section13/hooks/useFetch";
 export default function Day() {
 
   const { day } = useParams();//?
-  const wordList = dummy.words.filter(word => word.day === Number(day));
+  // const wordList = dummy.words.filter(word => word.day === Number(day));
   //dummy데이터에서 words의 day의 값을 wordList에 배열로 가지고 온다. 이후 Number(day)의 값과 같은 값들을 필터로 거쳐서 wordList에 담겨지고 해당 단어들이 페이지에 기제 된다.
+  // const [words, setWords] = useState([]);
+
+  // useEffect(() => {
+  //   fetch(`http://localhost:3001/words?day=${day}`)
+  //     .then(res => {
+  //       return res.json()
+  //     })
+  //     .then(data => {
+  //       setWords(data);
+  //     })
+  // }, [day]);
+
+  const words = useFetch(`http://localhost:3001/words?day=${day}`)
+
   return <>
     <h2>Day {day}</h2>
     <table>
       <tbody>
-        {wordList.map(word => (
+        {words.map(word => (
           <Word word={word} key={word.id} />
         ))}
       </tbody>
--- React_clone/clone_voca/src/components/section9_10/DayList.js
+++ React_clone/clone_voca/src/components/section9_10/DayList.js
@@ -1,14 +1,52 @@
 import { Link } from "react-router-dom";
-import dummy from "../../db/data.json";
+// import { useEffect, useState } from "react";
+import useFetch from "../section13/hooks/useFetch";
 
 export default function DayList() {
-  console.log(dummy);
-  return <ul className="list_day">
-    {
-      dummy.days.map(day => (
-        <li key={day.id}>
-          <Link to={`/day/${day.day}`}>Day{day.day}</Link>
-        </li>
-      ))}
-  </ul>
+  const days = useFetch("http://localhost:3001/days");
+  // const [days, setDays] = useState([]);
+  // const [count, setCount] = useState(0);
+
+  // function onclick() {
+  //   setCount(count + 1);
+  // }
+
+  // function onclick2() {
+  //   setDays([
+  //     ...days,
+  //     {
+  //       id: Math.random(),
+  //       day: 1,
+  //     }
+  //   ]);
+  // }
+
+  // useEffect(() => {
+  //   fetch('http://localhost:3001/days')
+  //     .then(res => {
+  //       return res.json()
+  //     })// 여기서 res는 실제 json이 아니고 http인 것이다. 이것을 res로 받아서 .json파일로 변환하는 것이다.
+  //     .then(data => {
+  //       setDays(data);
+  //     })//이후에 해당 API의 데이터 값을 data 매개변수로 가지고 와 setDays(data)의 형태로 days 변수에 값을 넣는다. 그렇면 return문에 있는 days.map함수가 돌아가면서 해당 데이터의 배열을 나열 할 것이다.
+  // }, [])
+
+  // // 랜더링 변화는 돔의 반응 직후에 나온다.
+
+  return (
+    <>
+      < ul className="list_day" >
+        {
+          days.map(day => (
+            <li key={day.id}>
+              <Link to={`/day/${day.day}`}>Day{day.day}</Link>
+            </li>
+          ))
+        }
+      </ul >
+      {/* <button onClick={onclick}>{count}</button>
+      <button onClick={onclick2}>Day change</button> */}
+    </>
+  )
 }
+
