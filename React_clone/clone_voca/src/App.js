e44d2465c8216f43c92769f7c701209bb5dd0094
4BFC <4bee.code@gmail.com>
2023-08-18 18:31:47 +0900

    lecture section15

--- React_clone/clone_voca/src/App.js
+++ React_clone/clone_voca/src/App.js
@@ -3,7 +3,9 @@ import Day from './components/section9_10/Day';
 import DayList from './components/section9_10/DayList';
 import Header from './components/section9_10/Header';
 import EmptyPage from './components/section9_10/EmptyPage';
+import CreateWord from './components/section15/CreateWord'
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import CreateDay from './components/section15/CreateDay';
 function App() {
   return (
     <div className='App'>
@@ -13,6 +15,8 @@ function App() {
           <Route path="/" element={<DayList />} />
           <Route path="/day/:day" element={<Day />} />
           <Route path="*" element={<EmptyPage />} />
+          <Route path='/create_word' element={<CreateWord />} />
+          <Route path='/create_day' element={<CreateDay />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
