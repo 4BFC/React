d2efb1deba79b1d786b80b8326e9dd3fef63ddfe
4BFC <4bee.code@gmail.com>
2024-01-25 21:36:50 +0900

    update

--- React_ver_1/class_ver1/src/MyCal.js
+++ React_ver_1/class_ver1/src/MyCal.js
@@ -8,17 +8,25 @@ export default function MyCal() {
   const [b, setB] = useState("");
   const [result, setResult] = useState("Result")
   const [cal, setCal] = useState('+')
+  const [lis, setLis] = useState([])
 
   const Cal = (cal) => {
+    let resultValue;
     switch (cal) {
-      case '+': setResult(parseInt(a) + parseInt(b)); break;
-      case '-': setResult(parseInt(a) - parseInt(b)); break;
-      case 'x': setResult(parseInt(a) * parseInt(b)); break;
-      case '/': setResult(parseInt(a) / parseInt(b)); break;
-      case '%': setResult(parseInt(a) % parseInt(b)); break;
+      case '+': resultValue = parseInt(a) + parseInt(b); break;
+      case '-': resultValue = parseInt(a) - parseInt(b); break;
+      case 'x': resultValue = parseInt(a) * parseInt(b); break;
+      case '/': resultValue = parseInt(a) / parseInt(b); break;
+      case '%': resultValue = parseInt(a) % parseInt(b); break;
+      default: ;
     }
-    setA("")
-    setB("")
+    setResult(resultValue);
+    setLis((prevLis) => [...prevLis, a + cal + b + "=" + resultValue]);
+    //내부적으로 내부 값은 변동되고 UI로 랜더링 되는 구간의 변화(동적) 값을 useState를 사용해야한다.
+    //useState의 사용과 내부 변수의 활용 차이
+    //prevLis 처럼 값을 어떻게 가져 올 수 있는지..
+    setA("");//초기화
+    setB("");//초기화
   }
   function select(e) {
     setCal(e.target.value)
@@ -28,6 +36,19 @@ export default function MyCal() {
     setA("")
     setB("")
     setResult("Result");
+  }
+
+  // function delete_Fn() {
+  //   setLis((prevLis)=>[])
+  // }
+
+  function delete_Fn(indexToDelete) {//이부분 분석이 필요함
+    setLis((prevLis) => prevLis.filter((_, index) => index !== indexToDelete));
+  }
+
+
+  function delete_All() {
+    setLis([]);
   }
 
   return (
@@ -46,8 +67,20 @@ export default function MyCal() {
       <div>{cal}</div>
       <input value={b} placeholder="b" onChange={(e) => { setB(e.target.value) }}></input>
       <div>
-        <button onClick={() => { Cal(cal) }}>result</button>
+        <button onClick={() => {
+          Cal(cal)
+        }}>result</button>
         <button onClick={reset}>RESET</button>
+      </div>
+      <div>
+        <span>List</span>
+        <span><button onClick={() => { delete_Fn(0) }}>Delete</button></span>
+        <span><button onClick={delete_All}>Delete All</button></span>
+        <ul>
+          {lis.map((data, index) => (
+            <li key={index}>{data}</li>
+          ))}
+        </ul>
       </div>
     </>
   )
